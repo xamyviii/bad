b25bdfbcc253e91824bab6e4679d1e10a6744c22
Amit Kumar Das <122246487+xamyviii@users.noreply.github.com>
2024-09-29 16:18:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let a = amit;
+console.log(a);
+
+        window.onload = function() {
+            alert('Subscribe to our YouTube channel: https://www.youtube.com/channel/CHANNEL_ID');
+        };
